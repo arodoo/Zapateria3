025ddccf11776f293518512a2232bfc7242fa23e
arodoo <haro.em@hotmail.com>
2023-03-06 19:45:07 -0600

    First commit

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    public function index() {
+        return view('landing.pages.index');
+    }
+
+    public function catalogo(){
+        return view('landing.pages.catalogo');
+    }
+
+    public function mision(){
+        return view('landing.pages.misionVision');
+    }
+
+    public function acerca(){
+        return view('landing.pages.quienesSomos');
+    }
+
+    public function contacto(){
+        return view('landing.pages.contactanos');
+    }
+}
---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/usuarios', [App\Http\Controllers\Admin\HomeController::class, 'usuarios'])->name('admin.usuarios');
+    Route::get('/productosAdmi', [App\Http\Controllers\Admin\HomeController::class, 'productosAdmi'])->name('admin.productosAdmi');
+    Route::get('/categorias', [App\Http\Controllers\Admin\HomeController::class, 'categorias'])->name('admin.categorias');
+    Route::get('/productosUser', [App\Http\Controllers\Admin\HomeController::class, 'productosUser'])->name('admin.productosUser');
+    Route::get('/carrito', [App\Http\Controllers\Admin\HomeController::class, 'carrito'])->name('admin.carrito');
+});
